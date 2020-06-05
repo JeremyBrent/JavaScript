fac42ef72bb2526a16405d6b01c05aec913f2e8b
JeremyBrent <jeremybrentdata@gmail.com>
2020-06-05 19:30:35 -0400

    added date filter

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,19 +8,35 @@ tableData.forEach((ufoRecord) => {
     // console.log(ufoRecord)
     var trow = tbody.append("tr");
     Object.entries(ufoRecord).forEach(([key, value]) => {
-        console.log(key, value);
+        // console.log(key, value);
         var cell = trow.append("td");
         cell.text(value);
     })
 })
 
 
+var inputBtn = d3.select("#filter-btn")
 
+inputBtn.on("click",filterData);
 
+function filterData() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
+    tbody.text("")
+    
+    var inputDate = d3.select("#datetime").property("value")
 
-
-// // Append one cell for the student name
-// adding.append("td").text(tableData[0])
-// // Append one cell for the student grade
-// adding.append("td").text(tableData[1])
+    var dateFilter = tableData.filter(ufo => ufo.datetime === inputDate);
+    console.log(dateFilter)
+    
+    dateFilter.forEach((ufoRecord) => {
+        // console.log(ufoRecord)
+        var trow = tbody.append("tr");
+        Object.entries(ufoRecord).forEach(([key, value]) => {
+            // console.log(key, value);
+            var cell = trow.append("td");
+            cell.text(value);
+        })
+    })
+}
