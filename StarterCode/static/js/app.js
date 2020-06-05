d63d1e051ed9963c1cfb7a5a2ba87c8df3d13b86
JeremyBrent <jeremybrentdata@gmail.com>
2020-06-05 19:00:14 -0400

    added table data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
-
 // YOUR CODE HERE!
+
+var tbody = d3.select("#ufo-tablebody")
+
+tableData.forEach((ufoRecord) => {
+    // console.log(ufoRecord)
+    var trow = tbody.append("tr");
+    Object.entries(ufoRecord).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = trow.append("td");
+        cell.text(value);
+    })
+})
+
+
+
+
+
+
+
+// // Append one cell for the student name
+// adding.append("td").text(tableData[0])
+// // Append one cell for the student grade
+// adding.append("td").text(tableData[1])
