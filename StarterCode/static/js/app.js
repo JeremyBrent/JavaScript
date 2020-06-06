76f1d4e01a7eaea7149872b80bd009ddb02620ea
JeremyBrent <jeremybrentdata@gmail.com>
2020-06-05 20:16:50 -0400

    added all filters

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -14,23 +14,31 @@ tableData.forEach((ufoRecord) => {
     })
 })
 
+// var inputDate = d3.select("#datetime").on("change",filterData);
+// var inputCity = d3.select("#city").on("change",filterData)
+var inputBtn = d3.select("#filter-btn").on("click",filterData);
 
-var inputBtn = d3.select("#filter-btn")
 
-inputBtn.on("click",filterData);
+
+
 
 function filterData() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
     tbody.text("")
-    
-    var inputDate = d3.select("#datetime").property("value")
 
-    var dateFilter = tableData.filter(ufo => ufo.datetime === inputDate);
-    console.log(dateFilter)
-    
-    dateFilter.forEach((ufoRecord) => {
+    var inputDate = d3.select("#datetime").property("value")
+    var inputCity = d3.select("#city").property("value")
+    var inputState = d3.select("#state").property("value")
+    var inputCountry = d3.select("#country").property("value")
+    var inputShape = d3.select("#shape").property("value")
+
+    var filter = tableData.filter(ufo => ufo.datetime === inputDate || ufo.city === inputCity || ufo.state === inputState || ufo.country === inputCountry || ufo.shape === inputShape);
+    console.log(filter)
+
+   
+    filter.forEach((ufoRecord) => {
         // console.log(ufoRecord)
         var trow = tbody.append("tr");
         Object.entries(ufoRecord).forEach(([key, value]) => {
@@ -39,4 +47,20 @@ function filterData() {
             cell.text(value);
         })
     })
+
+    // var cityFilter = tableData.filter();
+    // console.log(cityFilter)
+    // cityFilter.forEach((ufoRecord) => {
+    //     // console.log(ufoRecord)
+    //     var trow = tbody.append("tr");
+    //     Object.entries(ufoRecord).forEach(([key, value]) => {
+    //         // console.log(key, value);
+    //         var cell = trow.append("td");
+    //         cell.text(value);
+    //     })
+    // })
+
+  
+    
+    
 }
